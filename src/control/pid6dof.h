276904ca6dbac22551888495b39e1130c310b9fd
SoniDavid <sonidavid46@gmail.com>
2025-02-23 19:49:28 -0600

    Fixed implementation linking errors

--- src/control/pid6dof.h
+++ src/control/pid6dof.h
@@ -5,32 +5,22 @@
 #include <vector>
 
 class PID6DOF {
-private:
-    Eigen::VectorXf u_;
-    std::array<PID,6> pids_; // {x,y,z,phi,theta,psi}
-
-    // PID pidX_{PID::defaultParams()};
-    // PID pidY_{PID::defaultParams()};
-    // PID pidZ_{PID::defaultParams()};
-    // PID pidPhi_{PID::defaultParams()}; // roll
-    // PID pidTheta_{PID::defaultParams()}; // pitch
-    // PID pidPsi_{PID::defaultParams()}; // yaw
-
-public:
-    PID6DOF::PID6DOF(const std::array<PIDParams, 6>& params) 
-                    : pids_{{params[0], params[1], params[2], 
-                    params[3], params[4], params[5]}} {}   
+    private:
+        Eigen::VectorXf u_;
+        std::array<PID,6> pids_; // {x,y,z,phi,theta,psi}
     
-    Eigen::VectorXf update(const Eigen::VectorXf& measurement, 
-                            const Eigen::VectorXf& desired);
-};
-
-Eigen::VectorXf PID6DOF::update(const Eigen::VectorXf& measurement, 
-                                const Eigen::VectorXf& desired) {
-    Eigen::VectorXf error = desired - measurement;
-    for (int i = 0; i < 6; i++) {
-        u_(i) = pids_[i].update(measurement(i), desired(i));
-    }
+    public:
+        PID6DOF(const std::array<PIDParams, 6>& params)
+            : u_(Eigen::VectorXf::Zero(6)),  // Initialize u_ vector
+              pids_{{params[0], params[1], params[2],
+                    params[3], params[4], params[5]}} {}
     
-    return u_;
-}
+        Eigen::VectorXf update(const Eigen::VectorXf& measurement,
+                              const Eigen::VectorXf& desired) {
+            Eigen::VectorXf error = desired - measurement;
+            for (int i = 0; i < 6; i++) {
+                u_(i) = pids_[i].update(measurement(i), desired(i));
+            }
+            return u_;
+        }
+    };
